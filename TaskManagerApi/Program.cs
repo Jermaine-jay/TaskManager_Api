ed071b6d433a4a37053c65f37b4777076561229d
Johnson Osita <80931376+Germaine-jay@users.noreply.github.com>
2023-12-10 00:50:04 +0100

    removed comments.

--- TaskManagerApi/Program.cs
+++ TaskManagerApi/Program.cs
@@ -24,8 +24,6 @@ builder.Services.AddRedisCache(builder.Configuration);
 builder.Services.AddHostedService<ConsumeScopedServiceHostedService>();
 
 
-// Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
