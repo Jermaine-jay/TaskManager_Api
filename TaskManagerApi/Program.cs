b9e3a6fe07001eaf1e08d1253deff0355190f1ae
Johnson Osita <80931376+Jermaine-jay@users.noreply.github.com>
2024-08-27 10:52:10 +0100

    chore: uncommented seeded data

--- TaskManagerApi/Program.cs
+++ TaskManagerApi/Program.cs
@@ -103,9 +103,9 @@ app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
 app.MapControllers();
 
 
-/*await app.SeedRole();
+await app.SeedRole();
 await app.ClaimSeeder();
 await app.SeededUserAsync();
 await app.ProjectSeeder();
-*/
+
 app.Run();
