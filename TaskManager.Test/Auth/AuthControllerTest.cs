0bc97c10fb7ef63ee05061d16aa4b4bea5ff3559
Johnson Osita <80931376+Jermaine-jay@users.noreply.github.com>
2025-02-02 16:27:49 +0100

    Chore: Made all test method variables type specific.

--- TaskManager.Test/Auth/AuthControllerTest.cs
+++ TaskManager.Test/Auth/AuthControllerTest.cs
@@ -18,7 +18,6 @@ namespace TaskManager.Test.Auth
         private Fixture _fixture;
         private readonly AuthController _authController;
 
-
         public AuthControllerTest()
         {
             _fixture = new Fixture();
@@ -27,16 +26,15 @@ namespace TaskManager.Test.Auth
 
         }
 
-
         [TestMethod]
         public async System.Threading.Tasks.Task AuthController_CreateUser_ShouldReturnOk()
         {
-            var user = _fixture.Create<UserRegistrationRequest>();
-            var request = _fixture.Create<SuccessResponse>();
+            UserRegistrationRequest user = _fixture.Create<UserRegistrationRequest>();
+            SuccessResponse request = _fixture.Create<SuccessResponse>();
             _mockAuthService.Setup(repo => repo.RegisterUser(user)).ReturnsAsync(request);
 
-            var result = await _authController.CreateUser(user);
-            var obj = result as OkObjectResult;
+            IActionResult result = await _authController.CreateUser(user);
+            OkObjectResult? obj = result as OkObjectResult;
 
             Assert.IsNotNull(obj.Value);
             Assert.AreEqual(request, obj.Value);
@@ -47,11 +45,11 @@ namespace TaskManager.Test.Auth
         [TestMethod]
         public async System.Threading.Tasks.Task AuthController_CreateUsers_ShouldThrowException_WhenAuthServiceThrowsException()
         {
-            var user = _fixture.Create<UserRegistrationRequest>();
+            UserRegistrationRequest user = _fixture.Create<UserRegistrationRequest>();
             _mockAuthService.Setup(repo => repo.RegisterUser(user)).ThrowsAsync(new InvalidOperationException("Failed to create user"));
 
-            var result = await _authController.CreateUser(It.IsAny<UserRegistrationRequest>());
-            var obj = result as OkObjectResult;
+            IActionResult result = await _authController.CreateUser(It.IsAny<UserRegistrationRequest>());
+            OkObjectResult? obj = result as OkObjectResult;
 
             Assert.IsNull(obj.Value);
             Assert.AreEqual(null, obj.Value);
@@ -62,12 +60,12 @@ namespace TaskManager.Test.Auth
         [TestMethod]
         public async System.Threading.Tasks.Task Authontroller_LoginUser_ShouldReturnOk()
         {
-            var request = _fixture.Create<LoginRequest>();
-            var response = _fixture.Create<AuthenticationResponse>();
+            LoginRequest request = _fixture.Create<LoginRequest>();
+            AuthenticationResponse response = _fixture.Create<AuthenticationResponse>();
             _mockAuthService.Setup(repo => repo.UserLogin(request)).ReturnsAsync(response);
 
-            var result = await _authController.LoginUser(request);
-            var obj = result as OkObjectResult;
+            IActionResult result = await _authController.LoginUser(request);
+            OkObjectResult? obj = result as OkObjectResult;
 
             Assert.IsNotNull(obj.Value);
             Assert.AreEqual(response, obj.Value);
@@ -78,12 +76,12 @@ namespace TaskManager.Test.Auth
         [TestMethod]
         public async System.Threading.Tasks.Task AdminController_LoginUser_ShouldThrowException_WhenAutherviceThrowsException()
         {
-            var request = _fixture.Create<LoginRequest>();
-            var response = _fixture.Create<AuthenticationResponse>();
+            LoginRequest request = _fixture.Create<LoginRequest>();
+            AuthenticationResponse response = _fixture.Create<AuthenticationResponse>();
             _mockAuthService.Setup(repo => repo.UserLogin(request)).ThrowsAsync(new InvalidOperationException("Invalid username or password"));
 
-            var result = await _authController.LoginUser(It.IsAny<LoginRequest>());
-            var obj = result as OkObjectResult;
+            IActionResult result = await _authController.LoginUser(It.IsAny<LoginRequest>());
+            OkObjectResult obj = result as OkObjectResult;
 
             Assert.IsNull(obj.Value);
             Assert.AreNotEqual(response, obj.Value);
